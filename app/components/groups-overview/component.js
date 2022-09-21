6fa130133ed547dec3030a2308e2d6875a7e32cf
andrewdschmidt <adschmidt@bsugmail.net>
2022-09-21 10:46:03 -0400

    Displaying the count of results per chat room

--- app/components/groups-overview/component.js
+++ app/components/groups-overview/component.js
@@ -2,9 +2,30 @@ import Component from '@ember/component';
 import Ember from 'ember';
 
 export default Component.extend({
-    classNames: ['groups-overview'],        
+    classNames: ['groups-overview'],
     groupService: Ember.inject.service(),
-    searchService: Ember.inject.service(),    
+    searchService: Ember.inject.service(),
+
+    sortResults(response) {
+        let results = response.results;
+
+        let sortedResults = [];
+
+        this.group.forEach(group => {
+            let filteredResults = results.filter(result => result.chatRoomName === group.name);
+
+            let helloThere = {
+                name: group.name,
+                results: filteredResults
+            }
+
+            sortedResults.push(helloThere);
+        });
+
+        this.set('sortedResults', sortedResults);
+    },
+
+
     actions: {
         searchRepo(searchValue) {
             // console.log(searchValue);
@@ -21,8 +42,9 @@ export default Component.extend({
             }
         },
         searchChatRooms() {
-            if (this.group && this.term && this.group.length > 0) {                
-                return this.get('searchService').searchChat(this.term, this.group).then(result => {                
+            if (this.group && this.term && this.group.length > 0) {
+                return this.get('searchService').searchChat(this.term, this.group).then(result => {
+                    this.sortResults(result);
                     this.set('resultsCount', result.total);
                     this.set('resultsContent', result.results);
                     return result;
